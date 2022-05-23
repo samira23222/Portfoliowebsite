9b6eb4dd255a3db41ed71fb92a829e5b851a6dee
samira23222 <93806757+samira23222@users.noreply.github.com>
2022-05-23 16:32:42 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector('.hamburger');
+const navlinks = document.querySelector('.navlinks');
+const links = document.querySelectorAll('.navlinks li')
+
+
+
+hamburger.addEventListener('click', () => {
+	navlinks.classList.toggle("open");
+});
