8cb0ad98ff86f48575b9e856902acbd25bc31487
samira23222 <93806757+samira23222@users.noreply.github.com>
2022-05-24 18:10:23 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navlinks = document.querySelector('.navlinks');
-const links = document.querySelectorAll('.navlinks li')
-
-
-
-hamburger.addEventListener('click', () => {
-	navlinks.classList.toggle("open");
-});
